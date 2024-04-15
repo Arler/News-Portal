d416a53208030d7e3d96f09367deb19f96c1b97d
arler <arler2w@gmail.com>
2024-04-15 14:06:00 +0300

    добавлена команда в manage.py для удаления всех новостей из указанной категории. Добавлена детальная информация о постах и фильтры в админ панели

--- NewsPortal/posts/admin.py
+++ NewsPortal/posts/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Post, Category, PostCategory
 
-admin.site.register(Post)
+
+def nullfy_rating(modeladmin, request, queryset):
+	# queryset - набор элементов выделенный галочками
+	queryset.update(rating=0)
+
+
+nullfy_rating.short_description = 'Обнулить рейтинг'
+
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'post_type', 'date', 'category', 'rating')
+	list_filter = ('author', 'post_type', 'rating', 'categories__category_name')
+	search_fields = ('title', 'categories__category_name')
+	actions = [nullfy_rating]
+
+	def category(self, obj):
+		return ", ".join([category.category_name for category in obj.categories.all()])
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(PostCategory)
