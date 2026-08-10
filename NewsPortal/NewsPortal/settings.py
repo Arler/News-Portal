dacc13bdb1ff791df8ee51d6d7b0aedc0f56e837
Arler <arler2w@gmail.com>
2026-08-10 18:06:35 +0300

    Изменён язык приложения на Русский

--- NewsPortal/NewsPortal/settings.py
+++ NewsPortal/NewsPortal/settings.py
@@ -91,7 +91,6 @@ CACHES = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -277,7 +276,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
